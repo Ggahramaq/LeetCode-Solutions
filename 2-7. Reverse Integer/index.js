35977a0c1f1679f75ab50c58a90b97427c507720
Ggahramaq <125120060+Ggahramaq@users.noreply.github.com>
2026-01-04 19:37:52 +0400

    Remove repeated toString() calls

--- 2-7. Reverse Integer/index.js
+++ 2-7. Reverse Integer/index.js
@@ -3,12 +3,13 @@
  * @return {number}
  */
 var reverse = function (y) {
-    let x = y.toString().replace(/\D/g, "");
+    y = y.toString()
+    let x = y.replace(/\D/g, "");
     let num = 0;
-    for (i in x.toString()) {
-        num += Number(x.toString()[x.toString().length - i - 1]) * (10 ** (x.toString().length - i - 1))
+    for (i in x) {
+        num += Number(x[x.length - i - 1]) * (10 ** (x.length - i - 1))
     }
-    if (y.toString()[0] == "-") {
+    if (y[0] == "-") {
         num = "-" + num;
     }
     if (Number(num) > 2 ** 31 || Number(num) < (2 ** 31 * -1)) {
@@ -17,4 +18,4 @@ var reverse = function (y) {
     return Number(num);
 };
 
-// console.log(reverse(-213)) <-- example of the use
+// console.log(reverse(-1237172)) <-- example of use
